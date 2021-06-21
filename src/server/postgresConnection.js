60f4cfc44d47ca886ef360cdaf7ca6761bed1197
Evator <sascha.weliki@googlemail.com>
2021-06-21 13:26:19 +0200

    ssl connection

--- src/server/postgresConnection.js
+++ src/server/postgresConnection.js
@@ -2,7 +2,7 @@ const { Pool, Client } = require('pg');
 const dbConnection = require('./dbConnection');
 const variables = require('./postgresVariables');
 const pg = require('pg-promise')()
-const connectionString = "postgresql://"+variables.user+":"+variables.password+"@"+variables.host+":"+variables.port+"/"+variables.database;
+const connectionString = "postgresql://"+variables.user+":"+variables.password+"@"+variables.host+":"+variables.port+"/"+variables.database+"?ssl=true";
 
 
 const db = pg(connectionString)
